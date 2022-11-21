e97be7706dbd9f47d38998dfa4ffd759cf51222f
Sakshi <sakshi.chandel@socialmediafreaks.com>
2022-11-21 15:29:49 +0530

    21-Nov-2022

--- beyond_aviation/homepage/admin.py
+++ beyond_aviation/homepage/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import *
 
 
+class SubSectionAdmin(admin.TabularInline):
+    model = SubSection
+    readonly_fields = ['section_icon_preview']
+
+
+class SectionAdmin(admin.ModelAdmin):
+    readonly_fields = ['section_image_preview']
+    inlines = [SubSectionAdmin, ]
+
+
 class ServiceAdmin(admin.ModelAdmin):
     readonly_fields = ['image_preview']
 
@@ -11,15 +21,9 @@ class ServiceOfferingAdmin(admin.ModelAdmin):
     readonly_fields = ['icon_preview']
 
 
-class SectionAdmin(admin.ModelAdmin):
-    readonly_fields = ['section_image_preview']
-
-
-class SectionIconAdmin(admin.ModelAdmin):
-    readonly_fields = ['section_icon_preview']
-
-
 admin.site.register(Service, ServiceAdmin)
 admin.site.register(ServiceOffering, ServiceOfferingAdmin)
 admin.site.register(Section, SectionAdmin)
-admin.site.register(SubSection, SectionIconAdmin)
+admin.site.site_header = "Go Above And Beyond"
+admin.site.site_title = "Go Above And Beyond admin site"
+admin.site.index_title = "Go Above And Beyond Admin"
