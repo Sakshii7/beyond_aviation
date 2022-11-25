db9766728d127e10aff5b3548c1de5e97df5b01e
Sakshi <sakshi.chandel@socialmediafreaks.com>
2022-11-25 18:17:19 +0530

    25-Nov-2022

--- beyond_aviation/homepage/urls.py
+++ beyond_aviation/homepage/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='Homepage'),
-    path('<slug:slug>', views.view_service, name='view_service'),
-    path('menu/<slug:slug_about>', views.view_menu, name='view_menu'),
+    path('', views.index, name='homepage'),
+    # path('<slug:slug>', views.view_service, name='view_service'),
+    path('homepage/<slug:slug_about>', views.view_menu, name='view_menu'),
 ]
